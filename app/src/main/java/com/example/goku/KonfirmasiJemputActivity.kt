da4301b3c2bf2b9c768e5ce4ed953cfd12a2686f
irham <irhamnajibazimulqowi@gmail.com>
2025-11-24 21:51:00 +0700

    input location page

---
+++ app/src/main/java/com/example/goku/KonfirmasiJemputActivity.kt
@@ -0,0 +1,11 @@
+package com.example.goku // Sesuaikan package
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KonfirmasiJemputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konfirmasi_jemput)
+    }
+}
