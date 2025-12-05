93ff44925edca7651db3a0e9c32d0214484beffc
radityaalfareza <radityareza263@gmail.com>
2025-12-05 13:55:10 +0700

    btn kembali lupa password

--- app/src/main/java/com/example/goku/LupaPasswordFragment.kt
+++ app/src/main/java/com/example/goku/LupaPasswordFragment.kt
@@ -5,55 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LupaPasswordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LupaPasswordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lupa_password, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_lupa_password, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LupaPasswordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LupaPasswordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // 1. Dapatkan referensi ke ImageButton Edit Profil
+        val btnKembali: Button = view.findViewById(R.id.btnKembali)
+
+        // 2. Tambahkan Listener untuk navigasi
+        btnKembali.setOnClickListener {
+            // Membuat instance dari LupaPasswordFragment
+            val ProfileFragment = ProfileFragment()
+
+            // Lakukan Fragment Transaction untuk mengganti Fragment
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ProfileFragment) // R.id.fragment_container adalah ID FrameLayout/tempat Fragment di MenuActivity
+                .addToBackStack(null) // Penting: Memungkinkan user menekan tombol back untuk kembali ke ProfilFragment
+                .commit()
+        }
+
+        return view
     }
 }
